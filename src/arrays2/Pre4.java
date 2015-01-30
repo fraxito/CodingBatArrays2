5cea1ececc1a94f10094343652bae708e7219ad2
Dani <Dani@Dani-PC>
2015-01-30 13:20:58 +0100

    Pre4 Termiado - Daniel Yubero Martínez

--- src/arrays2/Pre4.java
+++ src/arrays2/Pre4.java
@@ -1,17 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arrays2;
 
 /**
  *
- * @author Dani
+ * @author Daniel Yubero
  */
 public class Pre4 {
     
-    
+    public int[] pre4(int[] nums) {
+        int contar = 0;
+        for (int i = 0;( i < nums.length) && (nums[i] != 4); i++) {
+             contar++;
+        }
+        int[] resultado = new int[contar];
+        for (int i = 0; i < resultado.length; i++)
+            resultado[i] = nums[i];
+        return resultado;
+      }
 }
 
-//prueba
+
