fa738ded50cebd65c314b9b2932b091932b7d69d
xp <xp@android029>
2015-01-30 13:04:47 +0100

    3º commit (30-01-2015). Yoel Cano

--- src/arrays2/CenteredAverage.java
+++ src/arrays2/CenteredAverage.java
@@ -11,5 +11,28 @@ package arrays2;
  * @author xp
  */
 public class CenteredAverage {
+    public int centeredAverage(int[] nums) {
+int sum = 0;
+int numeroMayor = 0;
+int numeroMenor = 0;
+
+
+for (int i = 0; i < nums.length; i++){
+   if (nums[i]>=numeroMayor){
+    numeroMayor = nums[i];
+   }
+   if (nums[i]<=numeroMenor){
+    numeroMenor = nums[i];
+   }
+   
+}
+ 
+for (int j = 0; j < nums.length; j++){
+   sum = sum + nums[j];
+}
+ sum = sum - numeroMayor - numeroMenor; 
+       
+ return sum/(nums.length-2); 
+}
     
 }
