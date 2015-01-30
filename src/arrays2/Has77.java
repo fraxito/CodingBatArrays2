6ed9839e865bb1f42bf7225383c40f24d8a9e1bc
Sergio <Sergio@Sergio-PC>
2015-01-30 12:56:23 +0100

    Terminado Has77 soy un pro-Sergio Herrero Cruz

--- src/arrays2/Has77.java
+++ src/arrays2/Has77.java
@@ -12,15 +12,17 @@ package arrays2;
 public class Has77 {
      
     public boolean has77(int[] nums) {
-        int sietes = 0;
-        
-        for (int i=0; i < nums.length; i++){
-            if (nums[i] == 7 && nums[i - 1] == 7){
+        for (int i=0; i < nums.length - 1; i++){
+            if (nums[i] == 7 && nums[i + 1] == 7){
             return true;
             }
-           
-           
+        }
+        for (int x=0; x < nums.length - 2; x++){
+            if (nums[x] == 7 && nums[x + 2] == 7){
+            return true;
+            }
         }
         return false;
+        
     }
 }
