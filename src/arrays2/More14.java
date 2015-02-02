a0e4281094a9cba9eaba6a29e739569187d60c30
alvaro <alvaro@Alvaro>
2015-02-02 12:14:18 +0100

    02/02/15  MORE14 TERMINADO

--- src/arrays2/More14.java
+++ src/arrays2/More14.java
@@ -9,6 +9,16 @@ package arrays2;
  *
  * @author alvaro
  */
-public class More14 {
-    
+
+public class More14{
+public boolean More14(int[] nums) {
+  int ones = 0;
+    int fours = 0;
+ 
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) ones++;
+        if (nums[i] == 4) fours++;
+    }
+    return ones > fours;
+}
 }
