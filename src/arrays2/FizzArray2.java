f52360357c0e52051b6a1c99e071ad4505fc6e29
xp <xp@android0001>
2015-02-02 11:52:22 +0100

    añadido codigo FizzArray2

--- src/arrays2/FizzArray2.java
+++ src/arrays2/FizzArray2.java
@@ -6,19 +6,15 @@ package arrays2;
 
 /**
  *
- * @author Javier Fernandez-Trapa
+ * @author xp
  */
-
 public class FizzArray2 {
-   
     public String[] fizzArray2(int n) {
-       String[] array= new String[n];
-       for(int i=0;i<n;i++){
+        String[] array=new String[n];
+        for(int i=0;i<n;i++){
             array[i]=String.valueOf(i);
-       }
-        
-       return array;
+        }
+    return array;
     }
-}
- 
 
+}
