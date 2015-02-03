2d0d89e6284413e4fcbe191d0564e47a755be485
pablo <pablo@pablo-TOSH>
2015-02-03 14:01:28 +0100

    TERMINADO EL Sum13 - PABLO CARUS

--- src/arrays2/Sum13_1.java
+++ src/arrays2/Sum13_1.java
@@ -8,7 +8,7 @@ package arrays2;
  * @author pablo
  */
 public class Sum13_1 {
-  public int Sum13(int[] nums) {
+  public int Sum13_1(int[] nums) {
      int total = 0;
      for (int i = 0; i < nums.length; i++) {
         if (nums[i] != 13) total += nums[i];
