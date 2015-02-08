2d1fa66900948f4c19589447743abe7bc3133e2f
alvaro <alvaro@Alvaro>
2015-02-08 19:39:49 +0100

    08/02/15 de alvaro para hacer prueba

---
+++ src/arrays2/Sum28.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays2;
+
+/**
+ *
+ * @author alvaro
+ */
+public class Sum28 {
+     public boolean sum28(int[] nums) {
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++)
+        if (nums[i] == 2) sum += 2;
+    return sum == 8;
+}
+    
+}
